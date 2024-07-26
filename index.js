92b8f684d22cfcc7f134f1610339b856c3261688
Camilo Rodriguez Cuaran <camiepisode@outlook.com>
2024-07-25 22:51:50 -0500

    Fix: Exported BaseModel and Property

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { ServerManager } = require('./src/core/server.manager');
+const BaseModel = require('./src/core/utils/models/base.model');
+const { Property } = require('./src/core/utils/models/property.model');
 
 module.exports = {
-  ServerManager
+  ServerManager,
+  BaseModel,
+  Property
 };
